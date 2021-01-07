aeea4eec91fefbf51116bc929101c91f5f1f0d90
spenser6131 <brinkman.spenser@gmail.com>
2021-01-06 17:09:27 -0800

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet (str) {
+  const hour = str.split(':')[0];
+  if (hour < 12) {
+    return "Good Morning"
+  }
+  if (hour >= 12 && hour <= 17) {
+    return "Good Afternoon"
+  }
+  if (hour > 17) {
+    return "Good Evening"
+  }
+}
+
+function displayMessage (str) {
+  let el = document.querySelector("h1#greeting")
+  el.innerText = str
+}
